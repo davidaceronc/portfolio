00bf8d6d786e3d9f0f6c9689094c1f0062491990
David Ceron <davidaceronc@gmail.com>
2021-07-11 00:37:26 -0500

    Personal portfolio init with Nuxt and Vuetify

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Projects</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+@Component({
+  components: {},
+})
+export default class ProjectsPage extends Vue {
+}
+</script>
+
+<style lang="scss"></style>
