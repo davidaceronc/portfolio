ee85f653d7de25aea83acf3b7b1676b86481278e
David Ceron <davidaceronc@gmail.com>
2021-07-12 18:44:53 -0500

    Spotify clon and Portfolio added to projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -1,34 +1,52 @@
 export const projects = [
-  {
-    name: "Scalars",
-    description: `Application that offers Backend as a service, with a serverless aproach. Allows to create and manage databases, API, authentication flow and other backend services, all using a simple web console.`,
-    value: `Developed the web client that gives access to its services. Actively involved in the architectural design of the application.`,
-    technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL', 'AWS Lambda and S3'],
-    website: 'https://scalars.co/',
-    preview: 'Scalars.gif'
-  },
-  {
-    name: "Peektime",
-    description: `Social Network to find events, places and people in a city. `,
-    value: `Part of the team that developed the application. Lead the architectural design of the application.`,
-    technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
-    website: 'https://peektime.net/',
-    preview: 'Peektime.gif'
-  },
-  {
-    name: "Translate",
-    description: `Web Application that allows to create translations for your projects.`,
-    value: `Part of the team that developed the application. Involved in the architectural design of the application.`,
-    technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
-    repository: 'https://github.com/scalars/Translate',
-    preview: 'Translate.gif'
-  },
-  {
-    name: "Dodoers",
-    description: `Web Application that helps to find people to do tasks.`,
-    value: `Part of the team that developed the application. Involved in the architectural design of the application.`,
-    technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
-    website: 'https://dodoers.com/',
-    preview: 'Dodoers.gif'
-  },
+    {
+        name: "Scalars",
+        description: `Application that offers Backend as a service, with a serverless aproach. Allows to create and manage databases, API, authentication flow and other backend services, all using a simple web console.`,
+        value: `Developed the web client that gives access to its services. Actively involved in the architectural design of the application.`,
+        technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL', 'AWS Lambda and S3'],
+        website: 'https://scalars.co/',
+        preview: 'Scalars.gif'
+    },
+    {
+        name: "Peektime",
+        description: `Social Network to find events, places and people in a city. `,
+        value: `Part of the team that developed the application. Lead the architectural design of the application.`,
+        technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
+        website: 'https://peektime.net/',
+        preview: 'Peektime.gif'
+    },
+    {
+        name: "Translate",
+        description: `Web Application that allows to create translations for your projects.`,
+        value: `Part of the team that developed the application. Involved in the architectural design of the application.`,
+        technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
+        repository: 'https://github.com/scalars/Translate',
+        preview: 'Translate.gif'
+    },
+    {
+        name: "Dodoers",
+        description: `Web Application that helps to find people to do tasks.`,
+        value: `Part of the team that developed the application. Involved in the architectural design of the application.`,
+        technologies: ['Typescript', 'Vue', 'Nuxt', 'GraphQL'],
+        website: 'https://dodoers.com',
+        preview: 'Dodoers.gif'
+    },
+    {
+        name: "Spotify Clone",
+        description: `Spotify application with tracks search, reproduction and lyrics.`,
+        value: `Implemented this example to showcase backend with a serverless approach, using Node js and AWS Lambda.`,
+        technologies: ['Typescript', 'React', 'Node js', 'AWS Lambda and S3'],
+        website: 'http://spotify-clone-frontend.s3-website-us-east-1.amazonaws.com',
+        repository: 'https://github.com/davidaceronc/spotify-clone',
+        preview: 'Spotify.gif'
+    },
+    {
+        name: "Portfolio",
+        description: `Personal portfolio with a list of projects.`,
+        value: `Implemented to showcase the most relevant projects that i've worked on.`,
+        technologies: ['Typescript', 'Vue', 'Nuxt'],
+        website: 'http://david-personal-portfolio.s3-website-us-east-1.amazonaws.com',
+        repository: 'https://github.com/davidaceronc/portfolio',
+        preview: 'Portfolio.gif'
+    },
 ];
